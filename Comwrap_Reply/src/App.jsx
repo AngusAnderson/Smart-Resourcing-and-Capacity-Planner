8eda6f823375237d1c39ed7d6ae5aa581ab3f4e2
“Angus <2891984a@student.gla.ac.uk>
2025-11-03 15:23:13 +0000

    Added Header - non Responsive - Anderson

--- Comwrap_Reply/src/App.jsx
+++ Comwrap_Reply/src/App.jsx
@@ -8,26 +8,30 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='Header'>
+        <div className="Text-top_left">
+          <h1>Reply <span className='span-1_Comwrap'>Comwrap</span></h1>
+        </div>
+        <div className="Text-top_right">
+          <h1>John Doe</h1>
+        </div>
+
+        <div className="Text-middle">
+          <button class="button-access-ai">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+          </svg>
+            Access AI
+          </button>
+        </div>
+        
       </div>
-      <h1>Vite + React</h1>
+
+
+      
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
