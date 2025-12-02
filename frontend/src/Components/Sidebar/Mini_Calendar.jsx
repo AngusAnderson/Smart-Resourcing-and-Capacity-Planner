6a97f3e3133cf685c823d525131b02add0cf4e96
Charles Barr <2896588b@student.gla.ac.uk>
2025-12-02 16:32:26 +0000

    Fixed Small Calendar Updating Big Calendar

--- frontend/src/Components/Sidebar/Mini_Calendar.jsx
+++ frontend/src/Components/Sidebar/Mini_Calendar.jsx
@@ -13,11 +13,14 @@ import 'temporal-polyfill/global'
 import '@schedule-x/theme-default/dist/index.css'
 
 
-function Mini_Calendar() {
+function Mini_Calendar({ setSelectedDate }) {
   const eventsService = useState(() => createEventsServicePlugin())[0]
+
  
   const calendar = useCalendarApp({
-    views: [createViewMonthAgenda()],
+    views: [
+        createViewMonthAgenda() // Keep this if you want it as another view
+    ],
     events: [
       {
         id: '1',
@@ -26,6 +29,20 @@ function Mini_Calendar() {
         end: Temporal.PlainDate.from('2025-11-15'),
       },
     ],
+    callbacks: {
+      onClickAgendaDate: (date) => {
+        console.log("Date clicked:", date.toString());
+        if (setSelectedDate) {
+          console.log("Mini_Calendar sees setSelectedDate exists");
+          setSelectedDate(date);
+        } else {
+          console.error("setSelectedDate does not exist");
+        }
+ //   onSelectedDateUpdate: (date) => {
+
+
+    },
+  },
     plugins: [eventsService]
   })
  
@@ -37,9 +54,10 @@ function Mini_Calendar() {
     <div className="mini-calander">
       <ScheduleXCalendar calendarApp={calendar} />
       <hr className='heading-line'></hr>
-            
     </div>
   )
+
+
 }
  
 export default Mini_Calendar
--- frontend/src/Components/Sidebar/Sidebar.jsx
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -3,16 +3,14 @@ import '../../css/Sidebar/Sidebar.css'
 import Mini_Calendar from './Mini_Calendar'
 import Filter_Box from './Filter_Box'
 
-const Sidebar = ({ searchTerm, onSearchChange }) => {
+const Sidebar = ({ searchTerm, onSearchChange, setSelectedDate }) => {
 
-
+   console.log("Sidebar got setSelectedDate:", setSelectedDate);
 
     return (
         <div className="sidebar">
           {/* Code for mini calander in the sidebar */}
-            <Mini_Calendar />
-          
-
+            <Mini_Calendar setSelectedDate={setSelectedDate}/>         
           {/* Code for filter box in the sidebar */}
           <Filter_Box searchTerm={searchTerm} onSearchChange={onSearchChange}/>
           
