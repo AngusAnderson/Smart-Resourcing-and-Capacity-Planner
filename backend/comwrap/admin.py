3d31f2320c31e9a2f7bbd86e9befebb3021c8201
Ozias Silitonga <oziassilitonga@gmail.com>
2026-02-11 16:21:36 +0000

    added ResourceBU and ReplyEntity to Django Admin page

--- backend/comwrap/admin.py
+++ backend/comwrap/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Employee, Specialism, JobCode, ForecastEntry
+from .models import Employee, Specialism, JobCode, ForecastEntry, ResourceBusinessUnit, ReplyEntity
 # Register your models here.
 
 admin.site.register(Employee)
 admin.site.register(Specialism)
 admin.site.register(JobCode)
 admin.site.register(ForecastEntry)
+admin.site.register(ResourceBusinessUnit)
+admin.site.register(ReplyEntity)
